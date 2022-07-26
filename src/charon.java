bcf0cc06a6499144326e3eb960500d1b34cbc7c1
KMGeon <103854287+KMGeon@users.noreply.github.com>
2022-07-27 00:03:24 +0900

    Initial commit

---
+++ src/charon.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class charon {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+
+        List<String> list1 = new  ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+
+        
+    }
+}
