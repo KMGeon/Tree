78d48f665731cacd735b45cae8d74364b8c2e7b5
Mugeon Kim <103854287+KMGeon@users.noreply.github.com>
2022-07-28 12:28:31 +0900

    Add files via upload

---
+++ DbUtill/ScanUtil.java
@@ -0,0 +1,23 @@
+package DBUtil;
+
+import java.util.*;
+
+public class ScanUtil {
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static String next() {
+		return sc.next();
+	}
+	
+	public static String nextLine() {
+		return sc.nextLine();
+	}
+	
+	public static int nextInt() {
+		return sc.nextInt();
+	}
+	
+	public static int nextLInt(){
+		return Integer.parseInt(sc.nextLine());
+	}
+}
