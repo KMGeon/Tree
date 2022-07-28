2ffa62455202e4e647635e82aac7b3e7caec9350
Mugeon Kim <103854287+KMGeon@users.noreply.github.com>
2022-07-28 12:29:19 +0900

    Add files via upload

---
+++ Others/Start.java
@@ -0,0 +1,144 @@
+package Others;
+
+public class Start {
+	static void window() {
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　　" + Font.BACKGROUND_RED+ Font.FONT_RED + "■■■■■■" + Font.BACKGROUND_GREEN + Font.FONT_GREEN + "■■■■■■" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　　" + Font.BACKGROUND_RED+ Font.FONT_RED + "■■■■■■" + Font.BACKGROUND_GREEN + Font.FONT_GREEN + "■■■■■■" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　　" + Font.BACKGROUND_BLUE+ Font.FONT_BLUE + "■■■■■■" + Font.BACKGROUND_YELLOW + Font.FONT_YELLOW + "■■■■■■" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　　" + Font.BACKGROUND_BLUE+ Font.FONT_BLUE + "■■■■■■" + Font.BACKGROUND_YELLOW + Font.FONT_YELLOW + "■■■■■■" + Font.RESET + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　　　Avengers4 \t\t");
+	}
+	
+	public static void avengers() throws Exception {
+		for (int i = 0; i < 4; i++) {
+			window(); //1
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　|　|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //2
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //3
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //4
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //5
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　│" + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //6
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　│　│" + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //7
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　│　│　│" + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //8
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　│　│　│　│" + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+			window(); //9
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　|　|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+			System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+			Thread.sleep(200);
+			System.out.println();
+		}
+		window(); //1
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "　|　|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+		System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		Thread.sleep(200);
+		System.out.println();
+		window(); //2
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+		System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		Thread.sleep(200);
+		System.out.println();
+		window(); //3
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+		System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		Thread.sleep(200);
+		System.out.println();
+		window(); //4
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t　　 " + Font.BACKGROUND_GREEN + Font.FONT_WHITE + "　│　│　" + Font.BACKGROUND_WHITE + Font.FONT_WHITE + "|　|　" + Font.BACKGROUND_BLACK + "　\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLACK + Font.FONT_WHITE + "Copyright ⓒ Avengers Corporation\t" + Font.RESET);
+		System.out.print(Font.BACKGROUND_BLACK + "\t\t\t\t\t\t");
+		System.out.println();
+		Thread.sleep(2000);
+		for (int i = 1; i < 100; i++) {
+			System.out.println();
+		}
+		System.out.println(Font.BACKGROUND_BLUE + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLUE + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + Font.FONT_WHITE + "\t\t\t\t" + Font.BACKGROUND_RED+ Font.FONT_RED + "■■■■" + Font.BACKGROUND_GREEN + Font.FONT_GREEN +"■■■■" + Font.BACKGROUND_CYAN + "\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + Font.FONT_WHITE + "\t\t\t\t" + Font.BACKGROUND_BLUE+ Font.FONT_BLUE + "■■■■" + Font.BACKGROUND_YELLOW + Font.FONT_YELLOW +"■■■■" + Font.BACKGROUND_CYAN + "\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + Font.FONT_WHITE + "\t\t\t        Avengers4 \t");
+		System.out.println(Font.BACKGROUND_CYAN + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + Font.FONT_WHITE + "\t\tAvengers is starting up...\t");
+		System.out.println(Font.BACKGROUND_CYAN + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_CYAN + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLUE + "\t\t\t\t\t\t");
+		System.out.println(Font.BACKGROUND_BLUE + "\t\t\t\t\t\t" + Font.RESET);
+		Thread.sleep(5000);
+		for (int i = 1; i < 100; i++) {
+			System.out.println();
+		}
+	}
+}
