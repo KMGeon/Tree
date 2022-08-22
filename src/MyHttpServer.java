8d58daf356d06ac97e1fa0ed8fb1edae6869e555
Mugeon Kim <103854287+KMGeon@users.noreply.github.com>
2022-08-22 17:39:28 +0900

    Add files via upload

---
+++ src/MyHttpServer.java
@@ -0,0 +1,14 @@
+package kr.or.ddit.HTTP;
+
+/**
+ * 간단한 웹서버 예제
+ */
+public class MyHttpServer {
+    private final int port = 80;
+    private final String encodeing = "UTF-8";
+
+    //HTTP서버 시작...
+    public void start() {
+
+    }
+}
