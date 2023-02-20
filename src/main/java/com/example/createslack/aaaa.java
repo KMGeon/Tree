6b19a74e9864c52c87756731fc6dc36958f88375
KMGeon <pos04167@kakao.com>
2023-02-21 00:24:17 +0900

    Slack을 이용한 에러 모니터링

---
+++ src/main/java/com/example/createslack/aaaa.java
@@ -0,0 +1,15 @@
+//package com.example.createslack;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class aaaa {
+//
+//    @Scheduled(fixedRate = 3000)
+//    public void test(){
+//        log.info("aaaa");
+//    }
+//}
