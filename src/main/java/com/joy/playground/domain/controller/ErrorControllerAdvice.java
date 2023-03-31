29c740afa48f25546f0a28b9f43f15dd71193089
KMGeon <pos04167@kakao.com>
2023-03-31 19:40:46 +0900

    ✨feat: Slack Error 모니터링 구축

--- src/main/java/com/joy/playground/domain/controller/ErrorControllerAdvice.java
+++ src/main/java/com/joy/playground/domain/controller/ErrorControllerAdvice.java
@@ -3,6 +3,8 @@ package com.joy.playground.domain.controller;
 import com.joy.playground.global.exception.ErrorResponse;
 import com.joy.playground.global.exception.UserEmailDuplicationException;
 import com.joy.playground.global.exception.UserNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,23 +16,26 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ErrorControllerAdvice {
 
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserEmailDuplicationException.class)
-    public ErrorResponse handleUserEmailIsAlreadyExisted() {
+    public ErrorResponse handleUserEmailIsAlreadyExisted(UserEmailDuplicationException e) {
+        logger.error("User's email address is already existed:",e.getMessage());
         return new ErrorResponse("User's email address is already existed");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserNotFoundException.class)
-    public ErrorResponse userNotFoundException() {
+    public ErrorResponse userNotFoundException(UserNotFoundException e) {
+        logger.error("User's email address is already existed:",e.getMessage());
         return new ErrorResponse("User Not Found Exception");
     }
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(MissingRequestHeaderException.class)
-    public ErrorResponse missingRequestHeaderException() {
+    public ErrorResponse missingRequestHeaderException(MissingRequestHeaderException e) {
+        logger.error("User's email address is already existed:",e.getMessage());
         return new ErrorResponse("MissingRequestHeaderException");
     }
 
