f706426298b220a211261ea72b4a25a612b8de2f
KMGeon <pos04167@kakao.com>
2023-04-21 20:36:58 +0900

    배포 준비

--- src/main/java/com/example/deployaws/TestController.java
+++ src/main/java/com/example/deployaws/TestController.java
@@ -1,19 +1,26 @@
 package com.example.deployaws;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class TestController {
+    public final PostRepository postRepository;
 
-    @GetMapping("/test")
-    public String test() {
-        return "hello World";
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postRepository.save(post);
     }
 
-    @GetMapping("/test2")
-    public String test2() {
-        return "Jenkins CI/CD Test";
+    @GetMapping("/posts")
+    public List<Post> getPostList() {
+        return postRepository.findAll();
     }
 
 }
