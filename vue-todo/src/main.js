a85b670a14534e5e9a50dd877f2d2d72bad968d0
KMGeon <pos04167@kakao.com>
2023-05-11 21:00:07 +0900

    feat: component

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,10 +1,10 @@
-
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap-utilities.css"
 
 
 const app = createApp(App)
--- vue-todo/src/views/HomeView.vue
+++ vue-todo/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+    <div id="app">
+        <Header></Header>
+        <Input></Input>
+        <list></list>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from "@/App.vue";
+import Footer from "@/App.vue";
+import Input from "@/components/Input.vue";
+import List from "@/components/List.vue";
+
+export default {
+    components: {List, Input, Footer, Header},
+    'Header': Header,
+    'Footer': Footer
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
