e5ab6d9f74cf70f25f778b775047e0679f1fcc2d
KMGeon <pos04167@kakao.com>
2023-05-21 19:36:56 +0900

    feat: component

--- vue-todo/src/components/Input.vue
+++ vue-todo/src/components/Input.vue
@@ -1,33 +1,68 @@
 <template>
-  <div id="input">
-    <el-input type="text" v-model="newTodoItem" placeholder="일정을 입력하세요." style="width: 300px"/>
-    <el-button class="d-inline-block" v-on:click="addTodo" v-on:keyup.enter="addTodo">입력</el-button>
-  </div>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in propsdata" className="shadow" v-bind:key="todoItem.item">
+        <i className="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}"
+           v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+        <span className="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i className="removeBtn fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-  data: function () {
-    return {
-      newTodoItem: ""
+  props: ['propsdata'],
+  methods: {
+    removeTodo: function (todoItem, index) {
+      this.$emit('removeItem', todoItem, index);
+    },
+    toggleComplete: function (todoItem, index) {
+      this.$emit('toggleItem', todoItem, index);
     }
-  }, methods: {
-    addTodo: function () {
-      if (this.newTodoItem !== '') {
-        // this.$emit("이벤트이름",인자1,인자2)
-        this.$emit('addTodoItem', this.newTodoItem);
-        this.clearInput();
-      }
-    },
-    clearInput: function () {
-      this.newTodoItem = '';
-    },
   }
 }
 </script>
 
-<style>
-#input {
-  text-align: center;
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
 }
 </style>
--- vue-todo/src/views/HomeView.vue
+++ vue-todo/src/views/HomeView.vue
@@ -1,46 +1,76 @@
 <template>
   <div id="app">
-    <Header></Header>
-    <!--    v-on:하위 컴포넌트에서 발생시킨 이벤트 이름 = 현재 컴포넌트의 메서드 명"-->
-    <Input v-on:addOneItem="addOneItem"></Input>
-    <!--    v-bind:내여보낼 프롭스 속성 이름 = "현재 위치의 컴포넌트 데이터 속성  -->
-    <list v-bind:propsdata="todoItems"></list>
-    <Footer></Footer>
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
 <script>
-import Header from "@/App.vue";
-import Footer from "@/App.vue";
-import Input from "@/components/Input.vue";
+
+import Header from "@/components/common/Header.vue";
 import List from "@/components/List.vue";
+import Input from "@/components/Input.vue";
+import Footer from "@/components/common/Footer.vue";
 
 export default {
-
-  data: function () {
+  data: function() {
     return {
       todoItems: []
     }
   },
   methods: {
-    addOneItem: function () {
-      var obj = {completed: false, item: this.newTodoItem};
-      localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+    addOneItem: function(todoItem) {
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
     },
+    removeOneItem: function(todoItem, index) {
+      this.todoItems.splice(index, 1);
+      localStorage.removeItem(todoItem.item);
+    },
+    toggleOneItem: function(todoItem, index) {
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      this.todoItems = [];
+      localStorage.clear();
+    }
   },
-  created: function () {
+  created: function() {
     if (localStorage.length > 0) {
       for (var i = 0; i < localStorage.length; i++) {
-        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
       }
     }
   },
-  components: {List, Input, Footer, Header},
-  'Header': Header,
-  'Footer': Footer
+  components: {
+    TodoHeader: Header,
+    TodoInput: Input,
+    TodoList: List,
+    TodoFooter: Footer
+  }
 }
 </script>
 
 <style>
-
+body {
+  text-align: center;
+  background-color: #F6F6F8;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+}
 </style>
