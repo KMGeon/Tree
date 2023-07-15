227ef26c0a0edb0318d23b8c4a52981867a74be1
KMGeon <pos04167@kakao.com>
2023-07-15 20:22:06 +0900

    Initial commit

---
+++ src/main/java/com/example/gymbatch/GymBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymBatchApplication.class, args);
+    }
+
+}
