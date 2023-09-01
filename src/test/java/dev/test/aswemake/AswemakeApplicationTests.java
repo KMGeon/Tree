f96ac380dfb7c49006ce2257dc44fde0b2fcf2e9
charon <pos04167@kakao.com>
2023-09-01 11:33:20 +0900

    init : init project

---
+++ src/test/java/dev/test/aswemake/AswemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.test.aswemake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AswemakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
