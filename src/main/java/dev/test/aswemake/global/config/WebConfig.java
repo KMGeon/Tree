6dd4da45daeae8c0d35fb27e47ea9021da16e45c
charon <pos04167@kakao.com>
2023-09-07 05:17:01 +0900

    feat : jwt, security 구현

---
+++ src/main/java/dev/test/aswemake/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package dev.test.aswemake.global.config;
+
+
+import dev.test.aswemake.global.argument.IfLoginArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new IfLoginArgumentResolver());
+    }
+}
