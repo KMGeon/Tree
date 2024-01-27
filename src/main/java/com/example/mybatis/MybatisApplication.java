92d115d353b27f2ba7dbecf9101a59173061ffb2
mugeon <pos04167@kakao.com>
2024-01-27 22:24:23 +0900

    feat : InMemoryHttpExchangeRepository 추가

--- src/main/java/com/example/mybatis/MybatisApplication.java
+++ src/main/java/com/example/mybatis/MybatisApplication.java
@@ -3,6 +3,7 @@ package com.example.mybatis;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
@@ -24,5 +25,12 @@ public class MybatisApplication {
         Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*.xml");
         sessionFactory.setMapperLocations(res);
         return sessionFactory.getObject();
+    }
+
+
+    // capacity가 100개여서 기본덕으로 넣는다.
+    @Bean
+    public InMemoryHttpExchangeRepository inMemoryHttpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 }
