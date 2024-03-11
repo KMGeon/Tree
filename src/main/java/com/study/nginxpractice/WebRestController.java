d4aee013859e9dad2158be53d58befef27a32c7c
mugeon <pos04167@kakao.com>
2024-03-11 20:55:26 +0900

    init : nginx practice

---
+++ src/main/java/com/study/nginxpractice/WebRestController.java
@@ -0,0 +1,25 @@
+package com.study.nginxpractice;
+
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+public class WebRestController {
+
+    private final Environment env;
+
+    public WebRestController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
