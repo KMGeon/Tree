7138e6dca68cd2390aac4ec497d645aa93f72a7d
mugeon <pos04167@kakao.com>
2024-03-16 23:55:06 +0900

    commit

--- script2/start.sh
+++ script2/start.sh
@@ -1,28 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/nginxPractice/build/libs
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ubuntu/nginxPractice
 PROJECT_NAME=nginxPractice
 
 echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/bliud/libs/*.war $REPOSITORY/"
 
 cp $REPOSITORY/*.war $REPOSITORY/
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl nginxPractice | grep war | awk '{print $1}')
-
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
-
 echo "> 새 어플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.war | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
