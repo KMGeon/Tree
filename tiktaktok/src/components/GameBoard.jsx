ad9110fbafc64e2a32dd53e91d618d0212b1d9e4
KMGeon <pos04167@kakao.com>
2024-03-26 17:08:56 +0900

    TIL0326 - ReactCore - TikTakToc

--- tiktaktok/src/components/GameBoard.jsx
+++ tiktaktok/src/components/GameBoard.jsx
@@ -1,18 +1,17 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-];
-
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
--- tiktaktok/src/components/Player.jsx
+++ tiktaktok/src/components/Player.jsx
@@ -1,34 +1,43 @@
-import {useState} from "react";
+import { useState } from 'react';
 
-export default function Player({initialName, symbol}) {
-
-    const [playerName,  setPlayerName] = useState(initialName);
+export default function Player({
+                                   initialName,
+                                   symbol,
+                                   isActive,
+                                   onChangeName,
+                               }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
-    function handleChange(event){
+    function handleChange(event) {
         setPlayerName(event.target.value);
     }
 
-    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
 
-    if (isEditing){
-        editablePlayerName = <input type='text' required value={playerName} onChange={handleChange}/>
+    if (isEditing) {
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
+        // btnCaption = 'Save';
     }
 
     return (
-        <>
-            <li>
-                <span className='player'>
-                    {/*<span className='player-name'>{name}</span>*/}
-                    {editablePlayerName}
-                    <span className='player-symbol'>{symbol}</span>
-                </span>
-                <button onClick={handleEditClick}>{isEditing ? 'Save':'Edit'}</button>
-            </li>
-        </>
+        <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+      </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
     );
 }
