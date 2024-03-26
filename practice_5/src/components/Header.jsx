98e291eeae9a684d3d5470fcf40835e7782e1cd8
KMGeon <pos04167@kakao.com>
2024-03-26 18:03:39 +0900

    TIL0326 - React CSS

---
+++ practice_5/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <header id='header'>
+            <h1>Investment Calculator</h1>
+        </header>
+    );
+};
+
+export default MyComponent;
